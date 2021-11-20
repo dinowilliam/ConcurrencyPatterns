508a7012fb53946a3f39572d33bc476f01c5835b
William Ferreira de Paula <739447+dinowilliam@users.noreply.github.com>
2021-11-20 15:18:15 -0300

    Changes in the menu

--- ConcurrencyPatterns/Program.cs
+++ ConcurrencyPatterns/Program.cs
@@ -6,27 +6,28 @@ using System.Linq;
 namespace ConcurrencyPatterns {
     class Program  {
         static void Main(string[] args) {
-            DrawMainScreen();
-          
-            
+            DrawMainScreen();                      
         }
 
         public static void DrawMainScreen() {
 
             Console.Clear();
-            Console.WriteLine("|******************************************************************************|");
-            Console.WriteLine("|                       Exercises from Design Patterns                         |");
-            Console.WriteLine("|******************************************************************************|");
-            Console.WriteLine("| Options                                                                      |");
-            Console.WriteLine("|      1 - ActiveObject Concurrency Pattern                                    |");            
-            Console.WriteLine("|      2 - MonitorObject Concurrency Pattern                                   |");
-            Console.WriteLine("|      3 - Concurrency Pattern                                                 |");
-            Console.WriteLine("|      4 - Concurrency Pattern                                                 |");
-            Console.WriteLine("|      5 - Concurrency Pattern                                                 |");
-            Console.WriteLine("|      0 - Exit                                                                |");
-            Console.WriteLine("|******************************************************************************|");
-            Console.WriteLine("| Choose one option to continue...                                             |");
-            Console.WriteLine("|******************************************************************************|");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("|*********************************************************************************************************************|");
+            Console.WriteLine("|                                                  dinowilliam.com                                                    |");
+            Console.WriteLine("|*********************************************************************************************************************|");
+            Console.WriteLine("|                                        Exercises from Concurrency Patterns                                          |");
+            Console.WriteLine("|*********************************************************************************************************************|");
+            Console.WriteLine("|                                Options                                                                              |");
+            Console.WriteLine("|                                        1 - ActiveObject Concurrency Pattern                                         |");            
+            Console.WriteLine("|                                        2 - MonitorObject Concurrency Pattern                                        |");
+            Console.WriteLine("|                                        3 - Concurrency Pattern                                                      |");
+            Console.WriteLine("|                                        4 - Concurrency Pattern                                                      |");
+            Console.WriteLine("|                                        5 - Concurrency Pattern                                                      |");
+            Console.WriteLine("|                                        0 - Exit                                                                     |");
+            Console.WriteLine("|*********************************************************************************************************************|");
+            Console.WriteLine("| Choose one option to continue...                                                                                    |");
+            Console.WriteLine("|*********************************************************************************************************************|");
 
             var option = Console.ReadLine();
 
